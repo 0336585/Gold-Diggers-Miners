a823a04f928554fd8fb2be4866c9c3a93b687177
Luuk Raijmakers <55003116+Misterkuul@users.noreply.github.com>
2025-02-13 17:53:58 +0100

    Ammo and Weapon Prefabs
    Added:
    -Weapons now have ammo
    -Weapon prefabs are added

--- Assets/Scripts/Weapons/ProjectileShooting.cs
+++ Assets/Scripts/Weapons/ProjectileShooting.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class ProjectileShooter : MonoBehaviour
@@ -7,33 +8,64 @@ public class ProjectileShooter : MonoBehaviour
     [SerializeField] private Transform firePoint;
 
     [Header("Fire Modes")]
-    [SerializeField] private bool automaticFire;
-    [SerializeField] private bool multishotFire;
+    [SerializeField] private bool automaticFire = false;
+    [SerializeField] private bool multishotMode = false;
 
     [Header("General Settings")]
+    //TODO: Damage doesn't do anything yet
+    [SerializeField] private float damagePerShot = 20f;
     [SerializeField] private float projectileSpeed = 25f;
     [SerializeField] private float projectileLifetime = 0.3f;
     [SerializeField] private float fireRate = 0.4f;
     [SerializeField] private float randomFireVariation = 0.1f;
 
-    [Header("Multishot Settings")]
-    [SerializeField] private int bulletsPerShot = 5;  
-    [SerializeField, Range(0f, 360f)] private float spreadAngle = 25f;  
+    [Header("Ammo Settings")]
+    [SerializeField] private int ammoInGun = 6;
+    [SerializeField] private int reserveAmmo = 30;
+    //TODO: Add private max ammo that's a check for reserveAmmo while being the same amount 
+    [SerializeField] private float reloadTime = 2f;
 
+    [Header("Multishot Settings")]
+    [SerializeField] private int bulletsPerShot = 5;
+    [SerializeField, Range(0f, 360f)] private float spreadAngle = 20f;
+
+    private int currentAmmo;
+    private bool isReloading = false;
     private float nextFireTime = 0f;
+
+    private void Start()
+    {
+        currentAmmo = ammoInGun;  
+    }
 
     private void Update()
     {
+        if (isReloading) return; // Cannot fire while reloading
+
         // If automaticFire is true, shouldFire is true when "Fire1" is held down.
         // If automaticFire is false, shouldFire is true only when "Fire1" is first pressed.
         bool shouldFire = automaticFire ? Input.GetButton("Fire1") : Input.GetButtonDown("Fire1");
 
         if (shouldFire && Time.time >= nextFireTime)
         {
-            // A random range between -0.1 and 0.1, to simulate irregularities in these old guns
-            float randomDelay = Random.Range(-randomFireVariation, randomFireVariation);
-            nextFireTime = Time.time + fireRate + randomDelay;
-            Shoot();
+            if (currentAmmo > 0)
+            {
+                //An random range between - 0.1 and 0.1, too simulate irregularities in these old guns
+                float randomDelay = Random.Range(-randomFireVariation, randomFireVariation);
+                nextFireTime = Time.time + fireRate + randomDelay;
+                Shoot();
+                currentAmmo--;  
+            }
+            else
+            {
+                Debug.Log("Out of ammo! Press 'R' to reload.");
+            }
+        }
+
+        // Reload when "R" is pressed
+        if (Input.GetKeyDown(KeyCode.R) && currentAmmo < ammoInGun && reserveAmmo > 0)
+        {
+            StartCoroutine(Reload());
         }
     }
 
@@ -45,7 +77,7 @@ public class ProjectileShooter : MonoBehaviour
             return;
         }
 
-        if (multishotFire)
+        if (multishotMode)
         {
             // Shotgun mode: Fire multiple bullets in a spread
             for (int i = 0; i < bulletsPerShot; i++)
@@ -72,5 +104,23 @@ public class ProjectileShooter : MonoBehaviour
             }
             Destroy(projectile, projectileLifetime);
         }
+    }
+
+    private IEnumerator Reload()
+    {
+        isReloading = true;
+        Debug.Log("Reloading...");
+        yield return new WaitForSeconds(reloadTime);  
+
+        int ammoNeeded = ammoInGun - currentAmmo; // How many bullets we need to refill
+        int ammoToReload = Mathf.Min(ammoNeeded, reserveAmmo); // Take from total ammo
+
+        currentAmmo += ammoToReload; // Fill the gun with bullets
+        reserveAmmo -= ammoToReload; // Reduce total ammo
+
+        isReloading = false;
+        Debug.Log("Reload complete!");
+        Debug.Log("currentAmmo: " + currentAmmo);
+        Debug.Log("reserveAmmo: " + reserveAmmo);
     }
 }
