2a66e4cd3cd4fd98695e075a31d156944b047494
0336585 <0336585@student.rocvantwente.nl>
2025-02-24 20:22:31 +0100

    Player can mine now
    - Player has a arm that follows mouse pos
    - Tiles break when the player clicks on them
    - The tile that is going to get mined when you click get outlined

---
+++ Assets/Scripts/Player/PlayerMining.cs
@@ -0,0 +1,115 @@
+using UnityEngine;
+
+public class PlayerMining : MonoBehaviour
+{
+    private Camera mainCamera;
+    private GameObject lastOutlineObject = null; // Tracks the last outline object
+
+    [Header("References")]
+    [SerializeField] private GameObject arm;
+    [SerializeField] private GameObject outlinePrefab; // Prefab for the outline effect
+
+    [Header("Mining Info")]
+    [SerializeField] private float raycastDistance = 2f; // Distance of the raycast
+    [SerializeField] private LayerMask ignoredLayer;
+
+    void Start()
+    {
+        mainCamera = Camera.main;
+    }
+
+    void Update()
+    {
+        ArmRotation();
+        PerformRaycast(); // Always check for outlining
+        CheckMining();    // Checks for mining when clicking
+    }
+
+    private void ArmRotation()
+    {
+        Vector3 mousePosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+        mousePosition.z = 0;
+
+        Vector3 direction = (mousePosition - arm.transform.parent.position).normalized;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+        arm.transform.rotation = Quaternion.Euler(0, 0, angle + 90);
+    }
+
+    private void PerformRaycast()
+    {
+        Vector2 origin = transform.position;
+        Vector3 mousePosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+        mousePosition.z = 0;
+
+        Vector2 direction = (mousePosition - transform.position).normalized;
+
+        RaycastHit2D hit = Physics2D.Raycast(origin, direction, raycastDistance, ~ignoredLayer);
+
+        if (hit.collider != null)
+        {
+            NoteHealth noteHealth = hit.collider.GetComponent<NoteHealth>();
+
+            if (noteHealth != null) // Only outline tiles with NoteHealth
+            {
+                if (lastOutlineObject == null || lastOutlineObject.transform.position != hit.collider.transform.position)
+                {
+                    RemoveOutline(); // Remove old outline before adding a new one
+                    CreateOutline(hit.collider.gameObject);
+                }
+            }
+        }
+        else
+        {
+            RemoveOutline(); // Remove outline if nothing is hit
+        }
+
+        Debug.DrawRay(origin, direction * raycastDistance, Color.red);
+    }
+
+    private void CheckMining()
+    {
+        if (Input.GetMouseButtonDown(0)) // Left mouse button
+        {
+            Vector2 origin = transform.position;
+            Vector3 mousePosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+            mousePosition.z = 0;
+
+            Vector2 direction = (mousePosition - transform.position).normalized;
+
+            RaycastHit2D hit = Physics2D.Raycast(origin, direction, raycastDistance, ~ignoredLayer);
+
+            if (hit.collider != null)
+            {
+                Debug.Log("Raycast hit something");
+
+                NoteHealth noteHealth = hit.collider.GetComponent<NoteHealth>();
+                if (noteHealth != null)
+                {
+                    Debug.Log("Hit an object with NoteHealth: " + hit.collider.gameObject.name);
+                    noteHealth.DamageNote(1);
+                }
+            }
+
+            Debug.DrawRay(origin, direction * raycastDistance, Color.red, 0.2f);
+        }
+    }
+
+    private void CreateOutline(GameObject target)
+    {
+        if (outlinePrefab == null) return;
+
+        lastOutlineObject = Instantiate(outlinePrefab, target.transform.position, Quaternion.identity);
+        //lastOutlineObject.transform.localScale = target.transform.localScale * 1.1f; // Slightly bigger for the outline effect
+        lastOutlineObject.transform.SetParent(target.transform); // Attach it to the target
+    }
+
+    private void RemoveOutline()
+    {
+        if (lastOutlineObject != null)
+        {
+            Destroy(lastOutlineObject);
+            lastOutlineObject = null;
+        }
+    }
+}
