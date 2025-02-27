9e157b4ccbd514936254fcb5ceec8f90611bc145
Mert <0336585@student.rocvantwente.nl>
2025-02-27 10:57:59 +0100

    Merged weapon ui with hotbar

--- Assets/Scripts/Player/Hotbar.cs
+++ Assets/Scripts/Player/Hotbar.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 public class Hotbar : MonoBehaviour
 {
     private GameObject equipedItemGO;
-    private InventoryItem equipedItem;
+    public InventoryItem equipedItem;
     public InventoryItem EquipedItem
     {
         get { return equipedItem; }
@@ -25,19 +25,37 @@ public class Hotbar : MonoBehaviour
     [SerializeField] private GameObject pickaxe;
     [SerializeField] private GameObject revolver;
 
+    private List<GameObject> instantiatedItems = new List<GameObject>();
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         playerMining = GetComponent<PlayerMining>();
-        weaponUIManager = GetComponent<WeaponUIManager>(); 
+        weaponUIManager = GetComponent<WeaponUIManager>();
 
         for (int i = 0; i < hotbarUI.transform.childCount; i++)
         {
             hotbarSlots.Add(hotbarUI.transform.GetChild(i).transform.GetChild(0).GetComponent<Image>());
         }
 
-        EquipItem(hotbarItems[0]);
+        // Instantiate all items in the hotbar and store them in a list
+        foreach (var item in hotbarItems)
+        {
+            if (item != null)
+            {
+                GameObject instantiatedItem = Instantiate(item.prefab, itemHolder.transform);
+                instantiatedItem.SetActive(false); // Deactivate all items initially
+                instantiatedItems.Add(instantiatedItem);
+            }
+        }
 
+        // Equip the first item in the list
+        if (hotbarItems.Count > 0 && hotbarItems[0] != null)
+        {
+            EquipItem(hotbarItems[0]);
+        }
+
+        // Update the hotbar UI
         for (int i = 0; i < hotbarItems.Count; i++)
         {
             if (hotbarItems[i] == null) break;
@@ -74,11 +92,24 @@ public class Hotbar : MonoBehaviour
 
     private void EquipItem(InventoryItem _item)
     {
-        if (equipedItemGO == _item) return;
+        if (equipedItem == _item) return;
 
-        if (equipedItemGO)
-            Destroy(equipedItemGO);
+        // Deactivate all items
+        foreach (var item in instantiatedItems)
+        {
+            item.SetActive(false);
+        }
 
+        // Activate the equipped item
+        int index = hotbarItems.IndexOf(_item);
+        if (index != -1 && index < instantiatedItems.Count)
+        {
+            instantiatedItems[index].SetActive(true);
+            equipedItemGO = instantiatedItems[index];
+            equipedItem = _item;
+        }
+
+        // Update mining ability based on item type
         if (_item.itemType == ItemType.Tool)
         {
             playerMining.EnableMining(true);
@@ -88,10 +119,6 @@ public class Hotbar : MonoBehaviour
             playerMining.EnableMining(false);
             playerMining.RemoveOutline();
         }
-
-
-        equipedItemGO = Instantiate(_item.prefab, itemHolder.transform);
-        equipedItem = _item;
 
         weaponUIManager.ChangeToolUI(equipedItem);
     }
