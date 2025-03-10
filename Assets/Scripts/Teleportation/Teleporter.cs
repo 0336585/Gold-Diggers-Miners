2493800033fc0c09989490a23e3a77c706e100ec
Luuk Raijmakers <55003116+Misterkuul@users.noreply.github.com>
2025-03-10 18:01:53 +0100

    Minor Town Tweaks
    Added:
    -Town now has it's own post processing
    -Input for teleportation changed

---
+++ Assets/Scripts/Teleportation/Teleporter.cs
@@ -0,0 +1,45 @@
+using TMPro;
+using UnityEngine;
+
+public class Teleporter : MonoBehaviour
+{
+
+    private bool inTeleportRange;
+
+    [SerializeField] private GameObject keyPressPopUp;
+    [SerializeField] private GameObject target;
+    [SerializeField] private GameObject player;
+    private void Update()
+    {
+        if (inTeleportRange)
+            TeleportableState();
+    }
+    void TeleportableState()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Teleport();
+        }
+    }
+    void Teleport()
+    {
+        player.transform.position = target.transform.position;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.GetComponent<BasePlayer>())
+        {
+            inTeleportRange = true;
+            keyPressPopUp.SetActive(true);
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.GetComponent<BasePlayer>())
+        {
+            inTeleportRange = false;
+            keyPressPopUp.SetActive(false);
+        }
+    }
+}
