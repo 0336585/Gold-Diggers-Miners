62e1087b9166b19c90afb2aa32d5079be981154a
Mert <0336585@student.rocvantwente.nl>
2025-03-12 15:17:12 +0100

    Buttons in main menu work now

---
+++ Assets/Scripts/UI/ButtonManager.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour
+{
+    [SerializeField] private GameObject optionsMenu;
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Options()
+    {
+        optionsMenu.SetActive(true);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
