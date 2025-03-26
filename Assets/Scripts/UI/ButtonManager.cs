b721c4b64a28bd714387a7947117921d922cb0be
Luuk Raijmakers <55003116+Misterkuul@users.noreply.github.com>
2025-03-26 13:37:58 +0100

    There is a pause menu now

--- Assets/Scripts/UI/ButtonManager.cs
+++ Assets/Scripts/UI/ButtonManager.cs
@@ -1,18 +1,54 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
 public class ButtonManager : MonoBehaviour
 {
-    [SerializeField] private GameObject optionsMenu;
+    [SerializeField] private GameObject pauseMenu;
+    private bool isPaused = false;
+
+    private void Start()
+    {
+        MenuManager.Instance.OnMenuOpen += ClosePauseMenu;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            TogglePauseMenu();
+        }
+    }
+
+    public void TogglePauseMenu()
+    {
+        if (isPaused)
+        {
+            ClosePauseMenu();
+        }
+        else
+        {
+            OpenPauseMenu();
+        }
+    }
 
     public void StartGame()
     {
         SceneManager.LoadScene("Level");
     }
 
-    public void Options()
+    public void OpenPauseMenu()
     {
-        optionsMenu?.SetActive(true);
+        MenuManager.Instance.MenuEvent();
+        isPaused = true;
+        pauseMenu?.SetActive(true);
+    }
+
+    public void ClosePauseMenu()
+    {
+        MenuManager.Instance.MenuEventClosed();
+        isPaused = false;
+        pauseMenu?.SetActive(false);
     }
 
     public void QuitGame()
